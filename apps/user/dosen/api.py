96454862fe63fa38a03bf4f9a9fe8bb44b5ce94d
silvanus <silvanusprasetyo@gmail.com>
2026-01-09 11:45:32 +0700

    ADD: CRUD api controller dosen

--- apps/user/dosen/api.py
+++ apps/user/dosen/api.py
@@ -3,6 +3,7 @@ from .models import Dosen
 from core.jwt_auth import JWTAuth
 from core.permissions import admin_only
 from typing import Optional
+from django.shortcuts import get_object_or_404
 
 router = Router(auth=JWTAuth(), tags=["Dosen"])
 
@@ -14,18 +15,49 @@ class DosenIn(Schema):
 
 class DosenOut(Schema):
     id: int
-    user_id: int
     name: str
     nidn: str
     fakultas: str
-
-@router.post("/")
-def create_dosen(request, data: DosenIn):
-    admin_only(request)
-    dosen = Dosen.objects.create(**data.dict())
-    return {"id": dosen.id}
 
 @router.get("/", response=list[DosenOut])
 def list_dosen(request):
     return Dosen.objects.all()
 
+@router.post("/")
+def create_dosen(request, data: DosenIn):
+    admin_only(request)
+    payload = data.dict()
+    if not payload.get('user_id'):
+        payload.pop('user_id', None)
+    try:
+        mahasiswa = Dosen.objects.create(**payload)
+        return {"id": mahasiswa.id, "message": "Dosen berhasil dibuat"}
+    except Exception as e:
+        return {"error": str(e)}, 400
+
+@router.put("/{dsn_id}")
+def update_dosen(request, dsn_id: int, data: DosenIn):
+    admin_only(request)
+    dosen = get_object_or_404(Dosen, id=dsn_id)
+
+    payload = data.dict()
+    if not payload.get('user_id'):
+          payload.pop('user_id', None)
+    try:
+        for attr, value in payload.items():
+            setattr(dosen, attr, value)
+
+        dosen.save()
+        return {"message": "Data dosen berhasil diperbarui"}
+    except Exception as e:
+            return {"error": str(e)}, 400
+    
+@router.delete("/{dsn_id}")
+def delete_dosen(request, dsn_id: int):
+    admin_only(request)
+    try:
+        dosen = get_object_or_404(Dosen, id=dsn_id)
+        dosen.delete()
+        return {"message": "Data dosen berhasil dihapus"}
+    except Exception as e:
+        return {"error": str(e)}, 400
